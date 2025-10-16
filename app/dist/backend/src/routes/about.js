b71225a15f8a93a9acd47ca3c4881f6a596d5804
aj-ats <andowens@advancedtech.com>
2025-10-16 16:20:59 -0400

    fawwk

--- app/dist/backend/src/routes/about.js
+++
@@ -1,6 +0,0 @@
-// routes/about.ts
-import { Router } from "express";
-import { getAboutPage } from "../controllers/aboutController.js";
-const router = Router();
-router.get("/about", getAboutPage);
-export default router;
--- app/dist/backend/src/routes/cart.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { signCart } from "../controllers/cartController.js";
-const router = Router();
-// Define the routes for the cart
-// incoming json needs to be 
-router.get('/cart/signCart', signCart);
-export default router;
